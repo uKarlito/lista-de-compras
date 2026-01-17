4ddffa2e6880aae601f6dea50c955008f5098fce
uKarlito <dedepimpu@gmail.com>
2026-01-17 18:30:43 -0300

    Feat: Hora da criação do item e de riscar item ao comprar

--- index.js
+++ index.js
@@ -20,9 +20,35 @@ botaoAdicionar.addEventListener("click", (evento) => {
   const nomeItem = document.createElement("p");
   nomeItem.innerText = inputItem.value;
 
+  inputCheckbox.addEventListener("click", function() {
+    if (inputCheckbox.checked) {
+      nomeItem.style.textDecoration = "line-through";
+    } else {
+      nomeItem.style.textDecoration = "none";
+    }
+  })
+
   containerItemDaLista.appendChild(inputCheckbox);
   containerItemDaLista.appendChild(nomeItem);
 
   itemDaLista.appendChild(containerItemDaLista);
-  listaDeCompras.appendChild(itemDaLista);
+
+  const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+    weekday: "long",
+  });
+  const data = new Date().toLocaleDateString("pt-BR");
+  const hora = new Date().toLocaleTimeString("pt-BR", {
+    hour: "numeric",
+    minute: "numeric"
+  });
+
+  const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+  const itemData = document.createElement("p");
+  itemData.innerText = dataCompleta;
+  itemData.classList.add("texto-data");
+  itemDaLista.appendChild(itemData);
+
+  listaDeCompras.appendChild(itemDaLista);  
+
+
 });
